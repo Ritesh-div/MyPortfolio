7261573a7420cb5694eb903bf937ffe79c9d6903
Ritesh-div <riteshkumarsahu1345@gmail.com>
2025-07-25 09:10:43 +0530

    Fix TypeScript and gsap import

--- src/mycomponents/hero.tsx
+++ src/mycomponents/hero.tsx
@@ -2,42 +2,46 @@ import StarBlinking from "./StarBlinking";
 import Navbar from "./Navbar";
 import BorderWaveButton from "./BorderWaveButton";
 import { useGSAP } from "@gsap/react";
+
 interface HeroProps {
   gsaptimeline: gsap.core.Timeline;
 }
 const Hero: React.FC<HeroProps> = ({ gsaptimeline }) => {
-    useGSAP(() => {
-    gsaptimeline.from(".dynamic", {
-      y: 30,
-      opacity: 0,
-      delay: 0.2,
-      duration: 0.5,
-    });
-    gsaptimeline.from(".name", {
-      x: -600,
-      opacity: 0,
-      delay: 0.2,
-      duration: 0.5,
-    });
-    gsaptimeline.from(".description", {
-      x: 600,
-      opacity: 0,
-      delay: 0.3,
-      duration: 0.5,
-    });
-        gsaptimeline.from(".connectBtn", {
-      y: 30,
-      opacity: 0,
-      delay: 0.3,
-      duration: 0.5,
-    });
-  });
+  useGSAP(() => {
+    gsaptimeline
+      .from(".dynamic", {
+        y: 30,
+        opacity: 0,
+        duration: 0.5,
+      })
+      .from(".name", {
+        x: -600,
+        opacity: 0,
+        duration: 0.5,
+      })
+      .from(".description", {
+        x: 600,
+        opacity: 0,
+        duration: 0.5,
+      })
+      .from(".connectBtn", {
+        y: 30,
+        opacity: 0,
+        duration: 0.5,
+      });
+  }, [gsaptimeline]);
+
   return (
-    <section id="home" className="relative min-h-screen flex flex-col items-center justify-center bg-black overflow-hidden px-6">
-      <Navbar gsaptimeline={gsaptimeline}/>
-         <StarBlinking />
+    <section
+      id="home"
+      className="relative min-h-screen flex flex-col items-center justify-center bg-black overflow-hidden px-6"
+    >
+      <Navbar gsaptimeline={gsaptimeline} />
+      <StarBlinking />
       <div className="relative z-10 text-center ">
-        <p className="text-xs text-gray-300 dynamic">DYNAMIC WEB MAGIC WITH REACT.JS</p>
+        <p className="text-xs text-gray-300 dynamic">
+          DYNAMIC WEB MAGIC WITH REACT.JS
+        </p>
         <h1 className=" text-3xl md:text-5xl font-bold text-white name">
           Hi, I'm <span className="">Ritesh</span>
         </h1>
@@ -51,7 +55,6 @@ const Hero: React.FC<HeroProps> = ({ gsaptimeline }) => {
         <div className="connectBtn">
           <BorderWaveButton />
         </div>
-        
       </div>
     </section>
   );
